9adb0e485e8e596becc22a2d766f404d8a1c9f7f
Achmad Mahardi <achmad@mahardi.me>
2014-11-15 01:38:19 +0700

    Now we minimize the js and chosen the old path instead. * hope it i'll be better * oh, also maybe removing backbone & sons are neat solutions.

--- src/SAR/routers/Silabus.router.php
+++ src/SAR/routers/Silabus.router.php
@@ -24,44 +24,69 @@ $app->get('/matakuliah/:idMatkul/silabus', $authenticate($app), function ($idMat
     $currPath = $app->request()->getPath();
     $kategori = new Kategori();
     $app->render('pages/_silabus.twig', array(
+        'currPath' => $currPath
+    ));
+});
+
+$app->get('/matakuliah/:idMatkul/silabus/new', $authenticate($app), function ($idMatkul) use ($app) {
+    $currPath = $app->request()->getPath();
+    $kategori = new Kategori();
+    $app->render('pages/_silabus-new.twig', array(
+        'currPath' => $currPath
+    ));
+});
+
+$app->get('/matakuliah/:idMatkul/kompetensi', $authenticate($app), function ($idMatkul) use ($app) {
+    $currPath = $app->request()->getPath();
+    $kategori = new Kategori();
+    $app->render('pages/_kompetensi.twig', array(
+        'kategori' => $kategori->getAllKategori(),
+        'currPath' => $currPath
+    ));
+});
+
+$app->get('/matakuliah/:idMatkul/pustaka', $authenticate($app), function ($idMatkul) use ($app) {
+    $currPath = $app->request()->getPath();
+    $kategori = new Kategori();
+    $app->render('pages/_pustaka.twig', array(
         'kategori' => $kategori->getAllKategori(),
         'currPath' => $currPath
     ));
 });
 
 /** POST request on `/matakuliah/:idMatkul/silabus` */
-$app->post('/matakuliah/:idMatkul/silabus', function ($idMatkul) use ($app) {
-    $silabus = new Silabus();
-    if (!empty($_POST)) {
-        $pokokBahasan = $_POST['pokok-bahasan'];
-        $tujuan = $_POST['tujuan'];
-        $kompetensi = $_POST['kompetensi'];
-        // echo('$silabus->save('.$pokokBahasan.', '.$idMatkul.', '.$tujuan.')<br>');
-        foreach ($kompetensi as $key => $item) {
-            // echo('$kompetensi->save(idsilabus'.$kompetensi[$key]['text'].')<br>');
-        }
-        $app->response()->header('Content-Type', 'application/json');
-        // echo(json_encode($_POST));
-    } else {
-        echo 'ERROR';
-    }
-});
+// $app->post('/matakuliah/:idMatkul/silabus', function ($idMatkul) use ($app) {
+//     $silabus = new Silabus();
+//     if (!empty($_POST)) {
+//         $pokokBahasan = $_POST['pokok-bahasan'];
+//         $tujuan = $_POST['tujuan'];
+//         $kompetensi = $_POST['kompetensi'];
+//         // echo('$silabus->save('.$pokokBahasan.', '.$idMatkul.', '.$tujuan.')<br>');
+//         foreach ($kompetensi as $key => $item) {
+//             // echo('$kompetensi->save(idsilabus'.$kompetensi[$key]['text'].')<br>');
+//         }
+//         $app->response()->header('Content-Type', 'application/json');
+//         // echo(json_encode($_POST));
+//     } else {
+//         echo 'ERROR';
+//     }
+// });
 
-$app->get('/matakuliah/:idMatkul/silabus/edit', $authenticate($app), function ($idMatkul) use ($app) {
-    $currPath = $app->request()->getPath();
-    $silabus = new Silabus();
-    $result = $silabus->init($idMatkul);
-    $app->render('pages/_silabus.twig', array(
-        'warning' => true,
-        'tujuan' => $silabus->tujuan,
-        'kompetensi' => $silabus->kompetensi,
-        'pokokBahasan' => $silabus->pokokBahasan,
-        'pustaka' => $silabus->pustaka,
-        'currPath' => $currPath
-    ));
-});
+// $app->get('/matakuliah/:idMatkul/silabus/edit', $authenticate($app), function ($idMatkul) use ($app) {
+//     $currPath = $app->request()->getPath();
+//     $silabus = new Silabus();
+//     $result = $silabus->init($idMatkul);
+//     $app->render('pages/_silabus.twig', array(
+//         'warning' => true,
+//         'tujuan' => $silabus->tujuan,
+//         'kompetensi' => $silabus->kompetensi,
+//         'pokokBahasan' => $silabus->pokokBahasan,
+//         'pustaka' => $silabus->pustaka,
+//         'currPath' => $currPath
+//     ));
+// });
 
-$app->post('/matakuliah/:idMatkul/silabus/edit', function ($idMatkul) use ($app) {
-    $app->response()->header('Content-Type', 'application/json');
-    echo json_encode($_POST);
-});
+// $app->post('/matakuliah/:idMatkul/silabus/edit', function ($idMatkul) use ($app) {
+//     $app->response()->header('Content-Type', 'application/json');
+//     echo json_encode($_POST);
+// });
